4035e84220a85ae89e57e2f71ae4d75857926e3c
Alja Čekada <alja.cekada@gmail.com>
2023-09-06 19:51:55 -0700

    define board model

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   @Get()
-  getAllBoards() {
-    return this.boardsService.getAllBoards;
+  getAllBoards(): Board[] {
+    return this.boardsService.getAllBoards();
   }
 }
