00419162ca44a44f5fb7426b6707eef42083142f
Alja Čekada <alja.cekada@gmail.com>
2023-09-13 15:27:32 -0700

    create user auth function

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Body, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './auth-credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body() AuthCredentials: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(AuthCredentials);
+  }
+}
