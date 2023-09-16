bb5455fdc22d941be127052689e4bc15392b8693
Alja Čekada <alja.cekada@gmail.com>
2023-09-15 19:53:56 -0700

    user can delete posts

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -47,10 +47,13 @@ export class BoardsService {
     return found;
   }
 
-  async deleteBoard(id: number): Promise<void> {
-    const result = await this.boardRepository.delete(id);
+  async deleteBoard(id: number, user: User): Promise<void> {
+    const result = await this.boardRepository.delete({
+      id,
+      user: { id: user.id },
+    });
     console.log('result', result);
-
+  
     if (result.affected === 0) {
       throw new NotFoundException(`Can't find board with id ${id}`);
     }
